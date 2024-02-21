7a2a164b0273abf5b35eec4005414997b5a35969
AnnamPrasuna <annamprasuna15@gmail.com>
2024-02-21 13:07:20 +0530

    Add project files.

---
+++ BlazorFileDownload/wwwroot/download.js
@@ -0,0 +1,31 @@
+﻿// wwwroot/download.js
+
+/*window.downloadFile = function (url) {
+    var anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = ''; // Give a name to the downloaded file if needed
+    anchor.target = '_blank'; // Open in a new tab, if needed
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+};*/
+window.downloadFile = function (fileName, buffer) {
+    // Create a blob from the buffer
+    var blob = new Blob([buffer], { type: "application/octet-stream" });
+
+    // Create an object URL for the blob
+    var url = URL.createObjectURL(blob);
+
+    // Create a temporary anchor element to trigger the download
+    var anchor = document.createElement("a");
+    anchor.href = url;
+    anchor.download = fileName;
+
+    // Trigger the download by clicking the anchor element
+    document.body.appendChild(anchor);
+    anchor.click();
+
+    // Cleanup
+    document.body.removeChild(anchor);
+    URL.revokeObjectURL(url);
+};
